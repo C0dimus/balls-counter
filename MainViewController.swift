10c490b3a9b119d464b64dbc80bc18803264d3f8
Adam Szczuchniak <adam.szczuchniak@gmail.com>
2018-06-26 14:10:52 -0700

    setup project structure, created Podfile and .gitignore

---
+++ MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Balls Counter
+//
+//  Created by Mac on 6/26/18.
+//  Copyright © 2018 C0dimus. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # balls-counter
-App counting red and blue balls. Written in RxSwift with usage of mvvm pattern.
+App counting red and blue balls. Written in *RxSwift* with usage of *mvvm pattern*.
+
+### Installation tutorial:
+- App was created in Xcdoe 9.2 with Swift 4.0
+- Pods are ignored, so app will require  `pod install` before building
